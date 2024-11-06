6c8f449db4e8640f5cbf4f027cb562b818453723
Alexander Yavtuhovich <yavtik@gmail.com>
2024-11-06 15:46:04 +0200

    HW38 - Client & server implementation

--- reverse-length-client-tcp-net/src/main/java/telran/app/net/Main.java
+++ reverse-length-client-tcp-net/src/main/java/telran/app/net/Main.java
@@ -1,7 +1,56 @@
 package telran.app.net;
 
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashSet;
+
+import telran.net.TcpClient;
+import telran.view.InputOutput;
+import telran.view.Item;
+import telran.view.Menu;
+import telran.view.StandardInputOutput;
+
 public class Main {
+    static TcpClient tcpClient;
+    private static final HashSet<String> types = new HashSet<>(Arrays.asList("normal", "reverse", "length"));
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Item[] items = {
+                Item.of("start session", Main::startSession),
+                Item.of("exit", Main::Exit, true)
+        };
+        Menu menu = new Menu("Reverse-Length TCP Client Application", items);
+        menu.perform(new StandardInputOutput());
     }
+
+    static void startSession(InputOutput io) {
+        String host = io.readString("Enter hostname");
+        int port = io.readNumberRange("Enter port", "Wrong port", 3000, 50000).intValue();
+        tcpClient = new TcpClient(host, port);
+
+        Menu menu = new Menu("Run Session",
+                Item.of("Send request", Main::requestProcessing),
+                Item.ofExit());
+        menu.perform(io);
+    }
+
+    static void Exit(InputOutput io) {
+        if (tcpClient != null) {
+            try {
+                tcpClient.close();
+            } catch (IOException e) {
+                io.writeLine("Error while closing client: " + e);
+            }
+        }
+    }
+
+    static void requestProcessing(InputOutput io) {
+        String typeOptions = String.join(", ", types);
+        String type = io.readStringOptions("Enter request type (" + typeOptions + ")",
+                "Invalid type, please choose again", types);
+        String request = io.readString("Enter string for processing");
+        String response = tcpClient.sendAndReceive(type, request);
+        io.writeLine("Response: " + response);
+    }
+
 }
--- reverse-length-server-tcp-net/src/main/java/telran/app/net/Main.java
+++ reverse-length-server-tcp-net/src/main/java/telran/app/net/Main.java
@@ -1,7 +1,32 @@
 package telran.app.net;
 
+import telran.net.Protocol;
+import telran.net.Request;
+import telran.net.Response;
+import telran.net.ResponseCode;
+import telran.net.TcpServer;
+
 public class Main {
+    private static int SERVER_PORT = 5011;
+    private static TcpServer tcpServer;
+    private static final Protocol protocol = (Request request) -> {
+        record ResponseInfo(ResponseCode code, String data) {
+        }
+
+        String requestData = request.requestData();
+
+        ResponseInfo responseInfo = switch (request.requestType()) {
+            case "reverse" -> new ResponseInfo(ResponseCode.OK, new StringBuilder(requestData).reverse().toString());
+            case "length" -> new ResponseInfo(ResponseCode.OK, String.valueOf(requestData.length()));
+            case "normal" -> new ResponseInfo(ResponseCode.OK, requestData);
+            default -> new ResponseInfo(ResponseCode.WRONG_TYPE, "Unsupported request type");
+        };
+
+        return new Response(responseInfo.code(), responseInfo.data());
+    };
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        tcpServer = new TcpServer(protocol, SERVER_PORT);
+        tcpServer.run();
     }
 }
